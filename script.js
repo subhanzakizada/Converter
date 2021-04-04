46bb24ceaf0f8f397286645a302afa0ff79aa996
subhanzakizada <zakizadasubhan@gmail.com>
2021-04-04 13:28:06 -0700

    add pound conversion

---
+++ script.js
@@ -0,0 +1,53 @@
+// Elements
+const optionsParent = document.getElementById('options-parent')
+const optionsChildOne = document.getElementById('options-child-one')
+const optionsChildTwo = document.getElementById('options-child-two')
+const firstBox = document.getElementById('first-box')
+const secondBox = document.getElementById('second-box')
+
+const poundConversion = (converserBox, convertedBox, opt1, opt2) => {
+    // because when multiply empty string('') by something, it gives 0 
+    if (converserBox.value === '') {
+        convertedBox.value = ''
+        return
+    }
+
+    // this variable gets assigned inside of if & else conditions 
+    let conversion
+
+    // lb to kg and kg to lb conversion
+    if ((opt1.value === 'lb' && opt2.value === 'kg') || opt1.value === 'kg' && opt2.value === 'lb') {
+        if (opt1.value === 'lb')
+            // formula is multiply pound by 0.453592 to get in kg
+            conversion = +(converserBox.value * 0.453592).toFixed(6) // lb to kg
+        else /* if(opt1.value === 'kg') */
+            // formula is multiply kilogram by 2.20462
+            conversion = converserBox.value * 2.20462 // kg to lb
+    }
+
+    // pound to gram and gram to pound
+    else if ((opt1.value === 'lb' && opt2.value === 'gm') || (opt1.value === 'gm' && opt2.value === 'lb')) {
+        if (opt1.value === 'lb') conversion = converserBox.value * 453.592
+        else /* if(opt1.value === 'gm') */ conversion = converserBox.value / 453.592
+
+    }
+
+
+
+    // lb to mg and mg to lb
+    else if ((opt1.value === 'mg' && opt2.value === 'lb') || (opt1.value === 'lb' && opt2.value === 'mg')) {
+        // first box lb and second mg
+        if (opt1.value === 'lb') conversion = converserBox.value * 453592 // lb to mg
+        else /* if(opt1.value === 'mg') */ conversion = converserBox.value / 453592 // mg to lb
+    }
+    
+    // making 6 decimal points max, if necessary
+    // toFixed() makes the decimal points and putting + sign at first removes unnecessary zeros if there are any
+    convertedBox.value = +conversion.toFixed(6)
+}
+
+
+
+firstBox.addEventListener('keyup', () => poundConversion(firstBox, secondBox, optionsChildOne.selectedOptions[0], optionsChildTwo.selectedOptions[0]))
+
+secondBox.addEventListener('keyup', () => poundConversion(secondBox, firstBox, optionsChildTwo.selectedOptions[0], optionsChildOne.selectedOptions[0]))
